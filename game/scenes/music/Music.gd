768fc802449d8e99ec72d4ad5e5ed3186a2e01b3
Lucas Vale <araujo.lucasvale@gmail.com>
2020-07-12 03:54:32 -0300

    [Fix] Ajusting some asset load

--- game/scenes/music/Music.gd
+++ game/scenes/music/Music.gd
@@ -10,7 +10,8 @@ var colorsArray = [red, green, blue]
 
 var timestamps = {}
 
-export var transition_duration = 1.00
+export var transition_out_duration = 1.00
+export var transition_in_duration = 1.00
 export var transition_type = 1 # TRANS_SINE
 
 onready var tween_out = get_node("Tween")
@@ -57,14 +58,14 @@ func reset_audios():
 	
 func fade_out(stream_player):
 	# tween music volume down to 0
-	tween_out.interpolate_property(stream_player, "volume_db", 0, -80, transition_duration, transition_type, Tween.EASE_IN, 0)
+	tween_out.interpolate_property(stream_player, "volume_db", 0, -80, transition_out_duration, transition_type, Tween.EASE_IN, 0)
 	tween_out.start()
 	# when the tween ends, the music will be stopped
 
 func fade_in(stream_player, seek):
 	if !stream_player.playing:
 		stream_player.play(seek)
-		tween_in.interpolate_property(stream_player, "volume_db", -80, 0, transition_duration, transition_type, Tween.EASE_IN, 0)
+		tween_in.interpolate_property(stream_player, "volume_db", -80, 0, transition_in_duration, transition_type, Tween.EASE_IN, 0)
 		tween_in.start()
 
 func _on_Tween_tween_completed(object, key):
